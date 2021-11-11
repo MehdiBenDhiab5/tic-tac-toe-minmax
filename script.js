28246a80cfffdbfa10ca4821ed3184aa46ed5eda
Mehdi BEN DHIAB <mehdibendhiab5@gmail.com>
2021-11-11 12:26:00 +0100

    added basic gameplay functionality, without display to page, without adding to array

--- script.js
+++ script.js
@@ -2,68 +2,142 @@
 const gameControl = (()=>{
     const arrayNodes = document.querySelectorAll('.column')
 
-    //Game Board Module
-    const gameBoard = (()=>{ 
-        let arr = [1,2,3,4,5,6,7,8,9];
-    
-        const resetArray = ()=>{
-            arr = [];
 
-            arrayNodes.forEach((node)=>{
-                node.innerText = "";
-            })
-        }
-
-        const displayArray = ()=>{
-
-            arrayNodes.forEach((node,index)=>{
-                node.innerText = arr[index];
-            })
-        }
-
-        const takeInMove = (ind, symbol)=>{
-            arr[ind] = symbol;
-            displayArray();
-        }
-
-        return {
-            resetArray,
-            displayArray,
-            takeInMove,
-        }
-    
-    })();
-
-    //Player Module
-    const player = (()=>{
-
-    })();
-    
-    //Computer Module
-    const computer = (()=>{
-        
-        const randomMove = ()=>{
-            return Math.floor(Math.random()*10)
-        }
-
-        //Will include logic for MIN MAX Algorithm later on
-        const computerChoice = ()=>{
-            return randomMove()
-        }
-
-        return {
-            randomMove,
-            computerChoice,
-        }
-
-    })();
     
     //Game Module
     const game = (()=>{
-    
+
+        //Game Board Module
+        const gameBoard = (()=>{ 
+            let arr = [1,2,3,4,5,6,7,8,9];
+        
+            const resetArray = ()=>{
+                arr = [];
+
+                arrayNodes.forEach((node)=>{
+                    node.innerText = "";
+                })
+            }
+
+            const displayArray = ()=>{
+
+                arrayNodes.forEach((node,index)=>{
+                    node.innerText = arr[index];
+                })
+            }
+
+            const getMove = (ind, symbol)=>{
+                arr[ind] = symbol;
+                displayArray();
+            }
+
+            return {
+                resetArray,
+                displayArray,
+                getMove,
+            }
+        
+        })();
+
+        //Player Module
+        const player = (()=>{
+            let myChoice;
+
+            const _disablePlayerChoice = ()=>{
+                arrayNodes.forEach((node)=>{
+                    node.removeEventListener("click",_onNodeClick)
+                })
+                playerMadeAMove(myChoice)
+            }
+
+            const _onNodeClick = (e)=>{
+                myChoice = e.target.id.valueOf()
+                e.target.innerText = "X"
+                _disablePlayerChoice()
+            }
+            const _enablePlayerChoice = ()=>{
+                arrayNodes.forEach((node)=>{
+                    node.addEventListener("click",_onNodeClick)
+                })
+            }
+
+            const getMove = ()=>{
+                myChoice = -1;
+                _enablePlayerChoice()
+                //while player hasnt made a move, wait
+                // while(myChoice = -1){}
+                // return myChoice;
+            }
+
+            return {
+                getMove,
+            }
+
+        })();
+        
+        //Computer Module
+        const computer = (()=>{
+            
+            const randomMove = ()=>{
+                return Math.floor(Math.random()*9)
+            }
+
+            //Will include logic for MIN MAX Algorithm later on
+            const getMove = ()=>{
+                let myMove = randomMove()
+                computerMadeAMove(myMove)
+            }
+
+            return {
+                randomMove,
+                getMove,
+            }
+
+        })();
+
+        const gameOver = ()=>{
+            return false
+        }
+
+        let playerChoice;
+        let computerChoice;
+
+        const playerMadeAMove = (choice)=>{
+            if (gameOver()){
+
+            }else{
+                //prompt computer for move
+                setTimeout(() => {
+                    playerChoice = choice;
+                    computer.getMove()
+                }, 500);
+            }
+            
+        }
+
+        const computerMadeAMove = (choice)=>{
+            if (gameOver()){
+
+            }else{
+                //prompt player for move
+                computerChoice = choice
+                console.log("computer made a move: " + computerChoice)
+                player.getMove()
+            }
+            
+        }
+
+        const playAGame = ()=>{
+
+        }
+
+        return {
+            startAGame : player.getMove
+        }
+
     })();
 
     //testing
-    console.log(computer.computerChoice())
-
+    game.startAGame()
+    
 })();
