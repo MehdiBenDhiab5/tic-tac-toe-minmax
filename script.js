013ae4fa6d3af0c600ce1f3c0799f978e246dd09
Mehdi BEN DHIAB <mehdibendhiab5@gmail.com>
2021-11-13 16:25:37 +0100

    added 1st iteration of minmax algorithm for computer choice. it cannot lose but it sometimes chooses wasteful moves, and thus decrease its chance of winning aswell

--- script.js
+++ script.js
@@ -60,7 +60,12 @@ const gameControl = (()=>{
                 return -1
             }
 
+            const getArr = () =>{
+                return arr
+            }
+
             return {
+                getArr,
                 resetArray,
                 displayArray,
                 getMove,
@@ -113,12 +118,118 @@ const gameControl = (()=>{
                 return Math.floor(Math.random()*9)
             }
 
+            //returns "tie" if the board is filled up
+            //returns "X" if X won
+            //returns "O" if O won
+            // returns -1 if the game hasn't ended yet
+            const gameOver = (arr)=>{
+                for (let i=0; i<3; i++){
+                    if ( arr[3*i] !=3 && (arr[3*i] == arr[3*i+1]) && ( arr[3*i+1] == arr[3*i+2])){
+                        return arr[3*i]
+                    }
+                    if ( arr[i] !=3 && (arr[i] == arr[i+3]) && (arr[i+3] == arr[i+6])){
+                        return arr[i]
+                    }
+                }
+                if ( arr[0] !=3 && (arr[0] == arr[4]) && (arr[4] == arr[8])){
+                    return arr[0]
+                }
+                if ( arr[2] !=3 && (arr[2] == arr[4]) && (arr[4] == arr[6])){
+                    return arr[2]
+                }
+
+                if ( arr.find(elem => elem === 3) == undefined ) {
+                    return "tie"
+                }
+                return -1
+            }
+
+            //MIN MAX
+            const minmax =  (CurrArrayState, player)=>{
+                //if game has ended return 0 or 10 or -10
+                if (gameOver(CurrArrayState) == "tie") {
+                    return 0
+                }else if (gameOver(CurrArrayState) == "X"){
+                    return -10
+                }else if (gameOver(CurrArrayState) == "O"){
+                    return 10
+                //else if game hasnt ended yet
+                }else {
+                    let testArr;
+                    let bestValue;
+                    //initialize bestValue depending on the player, so we can compare it
+                    if (player == "X") {
+                        bestValue = 10
+                    }else {
+                        bestValue = -10
+                    }
+                    //for every element in the array
+                    for (let i=0; i<CurrArrayState.length; i++){
+                        //if the element isnt  marked / is empty
+                        if(CurrArrayState[i] == 3){
+                            //create a copy of the array, and add a player/computer mark at the index
+                            testArr = [...CurrArrayState] 
+                            testArr[i] = player
+
+                            //call minmax with the newly created array, and compare the value returned to the best value we have
+                            //if its better, then keep the value
+                            //"X" wants to get the lowest possible value, "O" wants the biggest
+                            if(player == "X"){
+                                moveValue = minmax(testArr,"O")
+                                if (moveValue < bestValue) {
+                                    bestValue = moveValue
+                                }
+                            }else{
+                                moveValue = minmax(testArr,"X")
+                                if (moveValue > bestValue) {
+                                    bestValue = moveValue
+                                }
+                            }
+                            testArr[i] = 3
+                        }
+                    }
+                    return bestValue
+                }
+            }
+
+            //call MINMAX
+            const getCorrectMove = ()=>{
+
+                //clones the array from the gameBoard component
+                let currArr = [...gameBoard.getArr()]
+
+                //initialize best value and best move
+                //best move being an index that we will return at the end of the function
+                //best value being 0 or 10 or -10 depending on who won
+                let bestValue = -20
+                let bestMove;
+                let minmaxResults;
+
+                gameBoard.getArr().forEach((elem,index)=>{
+                    //for each element in the array that is empty
+                    if (elem == 3){
+                        //call minmax function, with the currArray after adding an "O" at the current index
+                        //minmax will return 0 or 10 or -10
+                        currArr[index] = "O"
+                        minmaxResults = minmax(currArr,"X")
+                        //if minmax returned a better value, save that value for further comparison, and save the index of the move
+                        if (minmaxResults>bestValue){
+                            bestValue = minmaxResults
+                            bestMove = index
+                        }
+                        //reset the currArray to prepare it for the next iteration
+                        currArr[index] = 3
+                    }
+                })
+                return bestMove
+            }
+
             //Will include logic for MIN MAX Algorithm later on
             const getMove = ()=>{
                 let myMove;
-                do {
-                    myMove = randomMove()
-                } while (gameBoard.moveIsLegal(myMove) == false)
+                // do {
+                    myMove = getCorrectMove()
+                // } while (gameBoard.moveIsLegal(myMove) == false)
                 gameBoard.getMove(myMove,"O")
                 computerMadeAMove(myMove)
             }
@@ -137,7 +248,7 @@ const gameControl = (()=>{
                 //prompt computer for move
                 setTimeout(() => {
                     computer.getMove()
-                }, 500);
+                }, 200);
             }
         }
 
